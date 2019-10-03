bd71bf06b6d3c33e56f61c165dfeba2b8da91135
Guilherme Augusto Gomes Cunha <41303580+CunhaGuilhermeBR@users.noreply.github.com>
2019-10-03 09:46:37 -0300

    Add files via upload

---
+++ Vertice.java
@@ -0,0 +1,19 @@
+public class Vertice {
+
+    private String nome;
+    private int grau;
+    
+
+    public Vertice(String nome, int grau) {
+        this.nome = nome;
+        this.grau = grau;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getGrau() {
+        return grau;
+    }
+}
